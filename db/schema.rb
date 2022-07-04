ac608a8f77b29dc06af5d76a3a5999f16fdf6488
shuto1998 <shuto.y0319@gmail.com>
2022-07-04 11:47:39 +0000

    updata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_013539) do
+ActiveRecord::Schema.define(version: 2022_07_04_114451) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
